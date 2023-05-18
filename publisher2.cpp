85ce697bf444e114fdf84dbe10362475e473bc0a
kkshitij17 <125120391+kkshitij17@users.noreply.github.com>
2023-05-18 17:40:38 +0530

    Add files via upload

---
+++ publisher2.cpp
@@ -0,0 +1,32 @@
+#include <ros/ros.h>
+#include<std_msgs/String.h>
+#include "package1/custom.h"
+#include<sstream>
+#include<iostream>
+#include<cstdio>
+#include<bits/stdc++.h>
+#include<string>
+using namespace std;
+
+int main(int argc,char **argv)
+{
+ros::init(argc,argv,"Publisher2");
+ros::NodeHandle nh;
+ros::Publisher pub =nh.advertise<package1::custom>("data2",1000);
+ros::Rate loop_rate(10);cout<<"node has bee initialized\n";
+
+while(ros::ok())
+{
+package1::custom msg;
+string message;
+cout<<"ready to take input\n";
+getline(cin,message);
+cout<<"\" "<<message<<" \""<<"will be sent to 1\n";
+msg.name=message;
+msg.num="NUMBER";
+pub.publish(msg);
+ros::spinOnce();
+loop_rate.sleep();
+}
+return 0;
+}
---
+++ subscriber1.cpp
@@ -0,0 +1,22 @@
+#include <ros/ros.h>
+#include<std_msgs/String.h>
+#include "package1/custom.h"
+#include<sstream>
+#include<iostream>
+#include<cstdio>
+#include<bits/stdc++.h>
+#include<string>
+using namespace std;
+void cb(const package1::custom::ConstPtr& msg)
+{
+cout<<( msg->name.c_str())<<" "<<(msg->num.c_str())<<endl;
+}
+int main(int argc,char **argv)
+{
+    ros::init(argc,argv,"subscriber1");
+    ros:: NodeHandle nr;
+    ros:: Subscriber sub = nr.subscribe("data2",1000,cb);
+
+    ros::spin();
+    return 0;
+}
